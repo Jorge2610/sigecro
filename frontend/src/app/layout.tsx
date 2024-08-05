f2b64c8f60df95178b767a2882c007f037d9a956
Jorge2610 <jorge261093@gmail.com>
2024-08-05 19:56:55 -0400

    perf: se redujo el peso del logo

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="es" >
       <head>
-        <link rel="icon" href="/logo.png" sizes="any" />
+        <link rel="icon" href="/logo.webp" sizes="any" />
       </head>
       <body className="flex-col relative w-[100svw] min-h-[100svh] font-montserrat">
         <Navigation />
