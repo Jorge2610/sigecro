9da92ceffcd47fd0a498f9f5a82b6d718d403c52
Jorge2610 <jorge261093@gmail.com>
2024-08-28 10:24:57 -0400

    fix: imports del Popup

--- frontend/src/components/noticias/manual/NewsForm.tsx
+++ frontend/src/components/noticias/manual/NewsForm.tsx
@@ -5,201 +5,211 @@ import { capitalizeWords } from "@/lib/stringsUtil";
 import messages from "../newsMessages.json";
 
 import {
-  InputForm,
-  InputDateForm,
-  InputTextAreaForm,
-  InputSelectForm,
-  InputFileForm,
-  InputTagsForm,
+    InputForm,
+    InputDateForm,
+    InputTextAreaForm,
+    InputSelectForm,
+    InputFileForm,
+    InputTagsForm,
 } from "./InputFormText";
 
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
-import Popup from "@/components/ui/popup";
+import { Popup } from "@/components/ui/popup";
 import FormSchema, { Data } from "./formSchema";
 import Preview from "./NewsPreview";
 
 const NewsForm = ({
-  preview,
-  setPreview,
-  setRecordType,
-  categories,
+    preview,
+    setPreview,
+    setRecordType,
+    categories,
 }: {
-  preview: boolean;
-  setPreview: React.Dispatch<React.SetStateAction<boolean>>;
-  setRecordType: React.Dispatch<React.SetStateAction<string>>;
-  categories: any;
+    preview: boolean;
+    setPreview: React.Dispatch<React.SetStateAction<boolean>>;
+    setRecordType: React.Dispatch<React.SetStateAction<string>>;
+    categories: any;
 }) => {
-  const [tags, setTags] = useState<string[]>([]);
-  const [data, setData] = useState<Data | null>(null);
-  const [imageURL, setImageURL] = useState<null | string>(null);
-  const [duplicatedTags, setDuplicatedTags] = useState<boolean>(false);
+    const [tags, setTags] = useState<string[]>([]);
+    const [data, setData] = useState<Data | null>(null);
+    const [imageURL, setImageURL] = useState<null | string>(null);
+    const [duplicatedTags, setDuplicatedTags] = useState<boolean>(false);
 
-  useEffect(() => {
-    form.setValue("tags", tags);
-    duplicatedTags
-      ? form.setError("tags", { type: "manual", message: messages.tags.unique })
-      : form.clearErrors("tags");
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [tags, duplicatedTags]);
+    useEffect(() => {
+        form.setValue("tags", tags);
+        duplicatedTags
+            ? form.setError("tags", {
+                  type: "manual",
+                  message: messages.tags.unique,
+              })
+            : form.clearErrors("tags");
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [tags, duplicatedTags]);
 
-  const form = useForm<Data>({
-    resolver: zodResolver(FormSchema),
-    defaultValues: {
-      title: "",
-      content: "",
-      date: undefined,
-      source: "",
-      url: "",
-      summary: "",
-      image: undefined,
-      status: "draft",
-      tags: [],
-      category: categories[0],
-      user_id: "1",
-    },
-  });
-
-  /**
-   * Maneja el envío del formulario actualizando el estado del componente con los datos enviados.
-   *
-   * @param {z.infer<typeof FormSchema>} data - Los datos enviados por el formulario.
-   * @return {void}
-   */
-  const onSubmit = (data: Data): void => {
-    data.source = capitalizeWords(data.source);
-    setImageURL(data.image ? URL.createObjectURL(data.image) : null);
-    setData(data);
-    setPreview(true);
-    setRecordType("manual");
-  };
-
-  /**
-   * Resete el formulario al estado inicial, limpiando la imagen previa y estableciendo el modo de vista previa en falso.
-   *
-   * @return {void}
-   */
-  const comeBack = (): void => {
-    form.reset(data || {});
-    setImageURL(null);
-    setPreview(false);
-  };
-
-  /**
-   * Resetea el formulario a su estado inicial.
-   *
-   * @return {void}
-   */
-  const cleanForm = (): void => {
-    form.reset({
-      title: "",
-      content: "",
-      date: undefined,
-      source: "",
-      url: "",
-      summary: "",
-      image: undefined,
-      status: "draft",
-      tags: [],
-      category: categories[0],
-      user_id: "1",
+    const form = useForm<Data>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            title: "",
+            content: "",
+            date: undefined,
+            source: "",
+            url: "",
+            summary: "",
+            image: undefined,
+            status: "draft",
+            tags: [],
+            category: categories[0],
+            user_id: "1",
+        },
     });
-    setData(null);
-    setImageURL(null);
-  };
 
-  return (
-    <>
-      {!preview ? (
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-            <InputForm
-              name="title"
-              label={messages.title.label}
-              control={form.control}
-              placeholder={messages.title.placeholder}
-              max={255}
-            />
+    /**
+     * Maneja el envío del formulario actualizando el estado del componente con los datos enviados.
+     *
+     * @param {z.infer<typeof FormSchema>} data - Los datos enviados por el formulario.
+     * @return {void}
+     */
+    const onSubmit = (data: Data): void => {
+        data.source = capitalizeWords(data.source);
+        setImageURL(data.image ? URL.createObjectURL(data.image) : null);
+        setData(data);
+        setPreview(true);
+        setRecordType("manual");
+    };
 
-            <InputForm
-              name="source"
-              label={messages.source.label}
-              control={form.control}
-              placeholder={messages.source.placeholder}
-              max={64}
-            />
+    /**
+     * Resete el formulario al estado inicial, limpiando la imagen previa y estableciendo el modo de vista previa en falso.
+     *
+     * @return {void}
+     */
+    const comeBack = (): void => {
+        form.reset(data || {});
+        setImageURL(null);
+        setPreview(false);
+    };
 
-            <InputDateForm
-              name="date"
-              label={messages.date.label}
-              control={form.control}
-              placeholder={messages.date.placeholder}
-            />
+    /**
+     * Resetea el formulario a su estado inicial.
+     *
+     * @return {void}
+     */
+    const cleanForm = (): void => {
+        form.reset({
+            title: "",
+            content: "",
+            date: undefined,
+            source: "",
+            url: "",
+            summary: "",
+            image: undefined,
+            status: "draft",
+            tags: [],
+            category: categories[0],
+            user_id: "1",
+        });
+        setData(null);
+        setImageURL(null);
+    };
 
-            <InputTextAreaForm
-              name="content"
-              label={messages.content.label}
-              control={form.control}
-              rows={10}
-              placeholder={messages.content.placeholder}
-            />
+    return (
+        <>
+            {!preview ? (
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-6"
+                    >
+                        <InputForm
+                            name="title"
+                            label={messages.title.label}
+                            control={form.control}
+                            placeholder={messages.title.placeholder}
+                            max={255}
+                        />
 
-            <InputTextAreaForm
-              name="summary"
-              label={messages.summary.label}
-              control={form.control}
-              placeholder={messages.summary.placeholder}
-            />
+                        <InputForm
+                            name="source"
+                            label={messages.source.label}
+                            control={form.control}
+                            placeholder={messages.source.placeholder}
+                            max={64}
+                        />
 
-            <InputForm
-              name="url"
-              label={messages.url.label}
-              control={form.control}
-              placeholder={messages.url.placeholder}
-              max={300}
-            />
+                        <InputDateForm
+                            name="date"
+                            label={messages.date.label}
+                            control={form.control}
+                            placeholder={messages.date.placeholder}
+                        />
 
-            <InputSelectForm
-              name="category"
-              label={messages.category.label}
-              control={form.control}
-              placeholder={messages.category.placeholder}
-              array={categories}
-            />
+                        <InputTextAreaForm
+                            name="content"
+                            label={messages.content.label}
+                            control={form.control}
+                            rows={10}
+                            placeholder={messages.content.placeholder}
+                        />
 
-            <InputFileForm
-              name="image"
-              label={messages.image.label}
-              control={form.control}
-            />
+                        <InputTextAreaForm
+                            name="summary"
+                            label={messages.summary.label}
+                            control={form.control}
+                            placeholder={messages.summary.placeholder}
+                        />
 
-            <InputTagsForm
-              setDuplicatedTags={setDuplicatedTags}
-              control={form.control}
-              name="tags"
-              label={messages.tags.label}
-              tags={tags}
-              setTags={setTags}
-            />
+                        <InputForm
+                            name="url"
+                            label={messages.url.label}
+                            control={form.control}
+                            placeholder={messages.url.placeholder}
+                            max={300}
+                        />
 
-            <div className="flex justify-end gap-4">
-              <Popup
-                title={messages.popupCancel.title}
-                description={messages.popupCancel.description}
-                action={cleanForm}
-                href="/administrar-noticias"
-              >
-                <Button variant="outline">Cancelar</Button>
-              </Popup>
-              <Button type="submit"> Previsualizar </Button>
-            </div>
-          </form>
-        </Form>
-      ) : (
-        <Preview imageURL={imageURL} data={data as any} action={comeBack} />
-      )}
-    </>
-  );
+                        <InputSelectForm
+                            name="category"
+                            label={messages.category.label}
+                            control={form.control}
+                            placeholder={messages.category.placeholder}
+                            array={categories}
+                        />
+
+                        <InputFileForm
+                            name="image"
+                            label={messages.image.label}
+                            control={form.control}
+                        />
+
+                        <InputTagsForm
+                            setDuplicatedTags={setDuplicatedTags}
+                            control={form.control}
+                            name="tags"
+                            label={messages.tags.label}
+                            tags={tags}
+                            setTags={setTags}
+                        />
+
+                        <div className="flex justify-end gap-4">
+                            <Popup
+                                title={messages.popupCancel.title}
+                                description={messages.popupCancel.description}
+                                action={cleanForm}
+                                href="/administrar-noticias"
+                            >
+                                <Button variant="outline">Cancelar</Button>
+                            </Popup>
+                            <Button type="submit"> Previsualizar </Button>
+                        </div>
+                    </form>
+                </Form>
+            ) : (
+                <Preview
+                    imageURL={imageURL}
+                    data={data as any}
+                    action={comeBack}
+                />
+            )}
+        </>
+    );
 };
 
 export default NewsForm;
--- frontend/src/components/noticias/manual/NewsPreview.tsx
+++ frontend/src/components/noticias/manual/NewsPreview.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import messages from "../newsMessages.json";
-import Popup from "@/components/ui/popup";
+import { Popup } from "@/components/ui/popup";
 import { Data, createFormData } from "./formSchema";
 import { splitIntoParagraphs } from "@/lib/stringsUtil";
 import { useToast } from "@/components/ui/use-toast";
@@ -53,80 +53,80 @@ const Preview: React.FC<PreviewProps> = ({ imageURL, data, action }) => {
         }
     };
 
-  return (
-    <div className="flex flex-col gap-4">
-      <h3>{data?.category?.name}</h3>
-      <h2 className="text-3xl font-lora font-medium">{data?.title}</h2>
-      <div className="flex items-center">
-        <span className="material-symbols-outlined text-sig-text mr-2">
-          newspaper
-        </span>
-        {data?.source}
-        <span className="material-symbols-outlined text-sig-text ml-20 mr-2">
-          calendar_clock
-        </span>
-        {data?.date && format(data.date, "dd-MM-yyyy HH:mm")}
-      </div>
-      <Summary summary={data?.summary} />
-      {imageURL && (
-        <div className="w-full h-[300px]">
-          <Image
-            src={imageURL}
-            width={300}
-            height={200}
-            className="h-[300px] w-auto m-auto p-4"
-            alt="image"
-          />
+    return (
+        <div className="flex flex-col gap-4">
+            <h3>{data?.category?.name}</h3>
+            <h2 className="text-3xl font-lora font-medium">{data?.title}</h2>
+            <div className="flex items-center">
+                <span className="material-symbols-outlined text-sig-text mr-2">
+                    newspaper
+                </span>
+                {data?.source}
+                <span className="material-symbols-outlined text-sig-text ml-20 mr-2">
+                    calendar_clock
+                </span>
+                {data?.date && format(data.date, "dd-MM-yyyy HH:mm")}
+            </div>
+            <Summary summary={data?.summary} />
+            {imageURL && (
+                <div className="w-full h-[300px]">
+                    <Image
+                        src={imageURL}
+                        width={300}
+                        height={200}
+                        className="h-[300px] w-auto m-auto p-4"
+                        alt="image"
+                    />
+                </div>
+            )}
+            <div>
+                {parapraphs.map((paragraph, i) => {
+                    return (
+                        <p className="mb-4" key={i}>
+                            {paragraph}
+                        </p>
+                    );
+                })}
+            </div>
+            <div className="flex flex-wrap gap-2 mt-4">
+                {data?.tags?.map((tag) => (
+                    <Badge
+                        key={tag}
+                        variant="secondary"
+                        className="bg-sig-gray2 h-7 hover:bg-sig-gray2"
+                    >
+                        {tag}
+                    </Badge>
+                ))}
+            </div>
+            {data?.url && (
+                <div className="flex flex-row gap-4 align-middle">
+                    <span className="material-symbols-outlined">link</span>
+                    <a
+                        href={data?.url}
+                        target="_blank"
+                        className="underline flex align-middle"
+                    >
+                        {data?.url}
+                    </a>
+                </div>
+            )}
+            <div className="flex flex-row justify-end gap-4">
+                <Button onClick={action} variant="outline">
+                    {" "}
+                    Regresar{" "}
+                </Button>
+                <Popup
+                    title={messages.popupPublic.title}
+                    description={messages.popupPublic.description}
+                    href="/administrar-noticias"
+                    action={publicar}
+                >
+                    <Button>Publicar</Button>
+                </Popup>
+            </div>
         </div>
-      )}
-      <div>
-        {parapraphs.map((paragraph, i) => {
-          return (
-            <p className="mb-4" key={i}>
-              {paragraph}
-            </p>
-          );
-        })}
-      </div>
-      <div className="flex flex-wrap gap-2 mt-4">
-        {data?.tags?.map((tag) => (
-          <Badge
-            key={tag}
-            variant="secondary"
-            className="bg-sig-gray2 h-7 hover:bg-sig-gray2"
-          >
-            {tag}
-          </Badge>
-        ))}
-      </div>
-      {data?.url && (
-        <div className="flex flex-row gap-4 align-middle">
-          <span className="material-symbols-outlined">link</span>
-          <a
-            href={data?.url}
-            target="_blank"
-            className="underline flex align-middle"
-          >
-            {data?.url}
-          </a>
-        </div>
-      )}
-      <div className="flex flex-row justify-end gap-4">
-        <Button onClick={action} variant="outline">
-          {" "}
-          Regresar{" "}
-        </Button>
-        <Popup
-          title={messages.popupPublic.title}
-          description={messages.popupPublic.description}
-          href="/administrar-noticias"
-          action={publicar}
-        >
-          <Button>Publicar</Button>
-        </Popup>
-      </div>
-    </div>
-  );
+    );
 };
 
 interface Props {
