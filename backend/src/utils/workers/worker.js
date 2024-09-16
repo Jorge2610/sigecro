551b0d41201f5725033948049292ec9a49d54a5c
Jorge2610 <jorge261093@gmail.com>
2024-09-16 19:14:57 -0400

    feat: asisted record working with worker threads

---
+++ backend/src/utils/workers/worker.js
@@ -0,0 +1,24 @@
+import { parentPort } from "worker_threads";
+import { getData } from "../scraping/newsScraping.js";
+
+/**
+ * Handles messages received from the main thread.
+ * The message should contain a task name and the corresponding arguments.
+ * The worker executes the task and sends the result back to the main thread.
+ *
+ * @param {Object} message - The message received from the worker pool.
+ * @param {string} message.taskName - The name of the task to be executed.
+ * @param {Array<any>} message.args - The arguments to pass to the task function.
+ */
+parentPort.on("message", async ({ taskName, args }) => {
+    let result;
+    switch (taskName) {
+        case "getData":
+            console.log("args", ...args);
+            result = getData(...args);
+            break;
+        default:
+            result = null;
+    }
+    parentPort.postMessage(result);
+});
