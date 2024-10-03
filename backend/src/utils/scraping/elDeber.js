4d875126e4149640e06c80dc472a547d96f9d6fd
Jorge2610 <jorge261093@gmail.com>
2024-10-02 21:55:52 -0400

    fix: wrong date on el deber scraping.

--- backend/src/utils/scraping/elDeber.js
+++ backend/src/utils/scraping/elDeber.js
@@ -19,10 +19,10 @@ monthsMap.set("diciembre", "12");
  * @return {JSON} - La información de la noticia.
  */
 const getElDeberData = ($) => {
-  const title = $("h1").text().trim();
-  const dateTime = getDateTime($(".dateNote:first").text().trim());
-  const content = getContent($);
-  return { title: title, dateTime: dateTime, content: content };
+    const title = $("h1").text().trim();
+    const dateTime = getDateTime($(".dateNote:first").text().trim());
+    const content = getContent($);
+    return { title: title, dateTime: dateTime, content: content };
 };
 
 /**
@@ -32,15 +32,17 @@ const getElDeberData = ($) => {
  * @return {Date} - La fecha de publicación de la noticia.
  */
 const getDateTime = (text) => {
-  text = text.split(" ");
-  const day = text[0];
-  const month = monthsMap.get(text[2]);
-  const year = text[4][0] + text[4][1] + text[4][2] + text[4][3];
-  const hours =
-    text[5].length === 5 ? text[5][0] + text[5][1] : "0" + text[5][0];
-  const minutes =
-    text[5].length === 5 ? text[5][3] + text[5][4] : text[5][2] + text[5][3];
-  return new Date(`${year}-${month}-${day}T${hours}:${minutes}:00`);
+    text = text.split(" ");
+    const day = text[0].length > 1 ? text[0].length : "0" + text[0].length;
+    const month = monthsMap.get(text[2]);
+    const year = text[4][0] + text[4][1] + text[4][2] + text[4][3];
+    const hours =
+        text[5].length === 5 ? text[5][0] + text[5][1] : "0" + text[5][0];
+    const minutes =
+        text[5].length === 5
+            ? text[5][3] + text[5][4]
+            : text[5][2] + text[5][3];
+    return new Date(`${year}-${month}-${day}T${hours}:${minutes}:00`);
 };
 
 /**
@@ -50,18 +52,18 @@ const getDateTime = (text) => {
  * @return {Date} - El contenido de la noticia.
  */
 const getContent = ($) => {
-  const content = [];
-  $(".text-editor div > p, .text-editor div > h3").each((i, element) => {
-    const text = $(element).text().trim();
-    if (
-      element.attribs.style?.includes("margin:") ||
-      element.attribs.style?.includes("color:")
-    ) {
-      return;
-    }
-    text.length > 1 && !text.includes("<span") ? content.push(text) : "";
-  });
-  return content;
+    const content = [];
+    $(".text-editor div > p, .text-editor div > h3").each((i, element) => {
+        const text = $(element).text().trim();
+        if (
+            element.attribs.style?.includes("margin:") ||
+            element.attribs.style?.includes("color:")
+        ) {
+            return;
+        }
+        text.length > 1 && !text.includes("<span") ? content.push(text) : "";
+    });
+    return content;
 };
 
 export { getElDeberData };
