501dfe6b34e9468adadd1bfbe2f73d3b488a8ef2
josue rodriguez blanco <josuebrandon99@gmail.com>
2024-10-04 16:28:12 -0400

    style: hover in the urls

--- frontend/src/components/ui/card-news.tsx
+++ frontend/src/components/ui/card-news.tsx
@@ -13,20 +13,24 @@ const CardNews = ({ data }: CardNewsProps) => {
     const urlNews = `/noticias/${formantNewsTitle(data.title)}_${data.id}`;
     return (
         <div className="rounded-[16px] border border-slate-200 p-6 mb-4 h-full bg-white shadow-lg">
-            <a href={urlNews}>
+            <a href={urlNews} className="hover:text-sky-600">
                 <p className="text-2xl font-semibold">{data.title}</p>
             </a>
             <div>
                 <div className="flex flex-wrap gap-2 my-2">
                     <div className="flex space-x-2 mr-4">
-                        <span className="material-symbols-outlined select-none">
-                            library_books
-                        </span>
+                        <div className="flex">
+                            <p>Fuente original: </p>
+                            <span className="material-symbols-outlined select-none">
+                                library_books
+                            </span>
+                        </div>
+                        
                         {
                         data.url === "" ? (
                             <p>{data.source}</p>
                         ) : (
-                            <a href={data.url} className="underline underline-offset-1">
+                            <a href={data.url} className="underline underline-offset-1 hover:text-sky-600">
                             {data.source}
                             </a>
                         )
